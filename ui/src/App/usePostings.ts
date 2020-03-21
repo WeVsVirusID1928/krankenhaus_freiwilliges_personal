5082270f4962af25a5974e5e90e34bc69bb2a745
Rahul Rakshit <rahul.rakshit@posteo.net>
2020-03-21 17:31:33 +0100

    Add postings type and parse accordingly

--- ui/src/App/usePostings.ts
+++ ui/src/App/usePostings.ts
@@ -1,6 +1,13 @@
 import { useEffect, useState } from 'react';
 
-export function usePostings() {
+interface Posting {
+  title: string;
+  time: string;
+  comment: string;
+  contact: string;
+}
+
+export function usePostings(): [Posting[] | null, boolean, Error | null] {
   const [postings, setPostings] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -8,7 +15,7 @@ export function usePostings() {
   useEffect(() => {
     fetch('http://localhost:1357/postings')
       .then(response => response.json())
-      .then(apiResponse => setPostings(apiResponse))
+      .then(apiResponse => setPostings(apiResponse.postings))
       .then(() => setLoading(false))
       .catch(err => {
         setLoading(false);
