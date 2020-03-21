4ca3365170af423ccdbe3963be8542a8e19427b8
Andreas Odin Kammerloher <kammerloher.andreas@gmail.com>
2020-03-21 22:46:06 +0100

    Make config more permissive

--- backend/src/main/java/de/howtohelppeople/config/SecurityConfig.java
+++ backend/src/main/java/de/howtohelppeople/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/public/**", "/test", "/user/*").permitAll()
+                .antMatchers("/", "/public/**", "/test", "/user/*", "/posting/*", "/posting/at/*", "/**").permitAll()
                 .antMatchers("/users/**").hasAuthority("ADMIN")
                 .anyRequest().fullyAuthenticated()
                 .and()
