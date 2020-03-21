f7d39847f3f2a2814cd58db7b1db9ecdd0553fdb
Andreas Odin Kammerloher <kammerloher.andreas@gmail.com>
2020-03-21 19:29:18 +0100

    Add function to get all users

--- backend/src/main/java/de/howtohelppeople/services/UserService.java
+++ backend/src/main/java/de/howtohelppeople/services/UserService.java
@@ -1,8 +1,12 @@
 package de.howtohelppeople.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
 
 import de.howtohelppeople.entities.UserEntity;
 import de.howtohelppeople.repositories.UserRepository;
@@ -10,6 +14,8 @@ import de.howtohelppeople.repositories.UserRepository;
 @Service
 @Transactional
 public class UserService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
+
     private UserRepository userRepository;
 
     @Autowired
@@ -19,6 +25,11 @@ public class UserService {
 
     public UserEntity create(UserEntity user) {
         return userRepository.save(user);
+    }
+
+    public Collection<UserEntity> getAllUsers() {
+        LOGGER.debug("Getting all users");
+        return userRepository.findAll();
     }
 
     public String getUserStats(Long id) {
