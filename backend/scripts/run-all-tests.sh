04cd20adf878fded2a7eae48252feb14075b5ff1
Philipp Ludewig <diesdas@philippludewig.de>
2020-03-21 13:32:23 +0100

    Now the tests run in backend

--- backend/scripts/run-all-tests.sh
+++ backend/scripts/run-all-tests.sh
@@ -2,10 +2,11 @@
 set -e -o pipefail
 
 function main() {
+cd backend  
 run_maven test 
 }
 
 pushd "${PROJECT_DIR}" > /dev/null
-  source "scripts/lib.sh"
+  source ${PWD}"/backend/scripts/lib.sh"
   main
 popd > /dev/null
