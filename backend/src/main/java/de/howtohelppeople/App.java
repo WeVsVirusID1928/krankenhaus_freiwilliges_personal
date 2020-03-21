d5e5fc7afb370b33c1d2cf5ebd38c6bba059ea89
Andreas Odin Kammerloher <kammerloher.andreas@gmail.com>
2020-03-21 13:57:18 +0100

    Set up Spring boot dependencies and Application

--- backend/src/main/java/de/howtohelppeople/App.java
+++ backend/src/main/java/de/howtohelppeople/App.java
@@ -2,12 +2,14 @@ package de.howtohelppeople;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Test Commit!" );
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Test Commit!");
     }
 }
