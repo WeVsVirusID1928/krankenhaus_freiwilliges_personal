724017729b5c44345ec5a15b6333875f9ccfe5ba
Mona Fenzl <mona.fenzl@web.de>
2020-03-26 22:43:02 +0100

    add seed data for postgres
    Addresses KP-seed-data
    
    Co-authored-by: Mona Fenzl <mona.fenzl@web.de>

---
+++ backend/src/main/resources/data-postgres.sql
@@ -0,0 +1,9 @@
+INSERT INTO hospitals (id, name, street, zip_code, country, city, latitude, longitude, phone_number, fax_number, email) VALUES (1, 'München Klinik Bogenhausen', 'Englschalkinger Straße 77', '81925', 'Germany', 'München', 48.155329, 11.624908, '(089) 9270-0', '(089) 9270-2248', '');
+INSERT INTO hospitals (id, name, street, zip_code, country, city, latitude, longitude, phone_number, fax_number, email) VALUES (2, 'München Klinik Harlaching', 'Sanatoriumsplatz 2', '81545', 'Germany', 'München', 48.086267, 11.558030, '(089) 6210-0', '(089) 6210-2442', '');
+INSERT INTO hospitals (id, name, street, zip_code, country, city, latitude, longitude, phone_number, fax_number, email) VALUES (3, 'München Klinik Neuperlach', 'Oskar-Maria-Graf-Ring 51', '81737', 'Germany', 'München', 48.094045, 11.657814, '(089) 6794-0', '(089) 6794-2346', '');
+INSERT INTO hospitals (id, name, street, zip_code, country, city, latitude, longitude, phone_number, fax_number, email) VALUES (4, 'München Klinik Schwabing', 'Kölner Platz 1', '80804', 'Germany', 'München', 48.171715, 11.577708, '(089) 3068-0', '(089) 3068-3801', '');
+INSERT INTO hospitals (id, name, street, zip_code, country, city, latitude, longitude, phone_number, fax_number, email) VALUES (5, 'Klinik Thalkirchner Straße', 'Thalkirchner Straße 48', '80337', 'Germany', 'München', 48.128784, 11.564200, '(089) 5147-6010', '(089) 5147-6403', '');
+
+
+
+
