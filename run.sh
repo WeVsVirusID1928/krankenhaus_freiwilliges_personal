9f5c7f94943b7f21ec0ee96c71940b20a1167603
Philipp Ludewig <diesdas@philippludewig.de>
2020-04-11 14:20:28 +0200

    Fixed run script
    Addresses NW-01

--- run.sh
+++ run.sh
@@ -4,11 +4,12 @@ set -e -o pipefail
 
 function check-dependencies()
 {
-  mvn --version || RUN curl -f -o /opt/maven.tar.gz http://mirror.netcologne.de/apache.org/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz &&\
+  sudo apt update && sudo apt install -y cron openjdk-8-jdk
+  mvn --version || curl -f -o /opt/maven.tar.gz http://mirror.netcologne.de/apache.org/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz &&\
     [ $(md5sum /opt/maven.tar.gz | cut -d' ' -f1) = "89eea39183139e5f8a0c1601d495b3b6" ] && \
     tar xzf /opt/maven.tar.gz -C /opt && \
     ln -s /opt/apache-maven-3.5.4/bin/mvn /usr/bin
-  java --version || RUN curl -f https://download.java.net/java/ga/jdk11/openjdk-11_linux-x64_bin.tar.gz -o /tmp/openjdk-11+28_linux-x64_bin.tar.gz && \
+  java --version || curl -f https://download.java.net/java/ga/jdk11/openjdk-11_linux-x64_bin.tar.gz -o /tmp/openjdk-11+28_linux-x64_bin.tar.gz && \
     [ $(md5sum  /tmp/openjdk-11+28_linux-x64_bin.tar.gz | cut -d' ' -f1) = "ede2734bdadffa4c26f1db8df2d48fdc" ] && \
     tar xfvz /tmp/openjdk-11+28_linux-x64_bin.tar.gz --directory /usr/lib/jvm && \
     rm -f /tmp/openjdk-11+28_linux-x64_bin.tar.gz
